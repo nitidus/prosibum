772fd435f75223e2ff2f80dcff7197ac02d0cd88
Ali Alizade Haqiqi <alializade@peedes.com>
2018-11-05 22:52:55 +0330

    Some functions created for converting digits to persian and vice versa, converting Gregorian date to Jalali and vice versa and finally a function for showing price in a correct format

--- assets/modules/functions/prototypes.js
+++ assets/modules/functions/prototypes.js
@@ -5,6 +5,100 @@ import { countries as __COUNTRIES } from '../../flows/knowledge/index';
 import { name as __APP_NAME } from '../../../app.json';
 
 module.exports = {
+  _convertDigitsToEnglish: (digits) => {
+    return digits.replace(/[\u0660-\u0669]/g, function (c) {
+        return c.charCodeAt(0) - 0x0660;
+    }).replace(/[\u06f0-\u06f9]/g, function (c) {
+       return c.charCodeAt(0) - 0x06f0;
+   });
+  },
+  _convertDigitsToPersian: (digits) => {
+    return digits.replace(/[0-9]/g, function (c) {
+        return String.fromCharCode(c.charCodeAt(0) + 0x0630);
+    });
+  },
+  _convertGregorianToJalali: (gy, gm, gd) => {
+   g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+
+   if (gy > 1600){
+    jy = 979;
+    gy -= 1600;
+   }else{
+    jy = 0;
+    gy -= 621;
+   }
+
+   gy2 = (gm > 2)? (gy + 1): gy;
+   days = (365*gy) + (parseInt((gy2+3)/4)) - (parseInt((gy2 + 99) / 100)) +(parseInt((gy2 + 399) / 400)) - 80 + gd + g_d_m[gm - 1];
+   jy += 33 * (parseInt(days / 12053));
+   days %= 12053;
+   jy += 4 * (parseInt(days / 1461));
+   days %= 1461;
+
+   if (days > 365){
+    jy += parseInt((days - 1) / 365);
+    days = (days - 1) % 365;
+   }
+
+   jm = (days < 186)? 1 + parseInt(days/31): 7 + parseInt((days - 186) / 30);
+   jd = 1 + ((days < 186)? (days % 31): ((days - 186) % 30));
+
+   return {
+     year: jy,
+     month: jm,
+     day: jd
+   };
+ },
+ _convertJalaliToGregorian: (jy, jm, jd) => {
+   if (jy > 979){
+    gy = 1600;
+    jy -= 979;
+   }else{
+    gy = 621;
+   }
+
+   days = (365 * jy) + ((parseInt(jy / 33)) * 8) + (parseInt(((jy % 33) + 3) / 4)) + 78 + jd + ((jm < 7)? (jm - 1) * 31: ((jm - 7) * 30) + 186);
+   gy += 400 * (parseInt(days / 146097));
+   days %= 146097;
+
+   if (days > 36524){
+    gy += 100 * (parseInt(--days / 36524));
+    days %= 36524;
+
+    if (days >= 365) days++;
+   }
+
+   gy += 4 * (parseInt(days / 1461));
+   days %= 1461;
+
+   if (days > 365){
+    gy += parseInt((days - 1) / 365);
+    days = (days - 1) % 365;
+   }
+
+   gd = days + 1;
+   sal_a = [0, 31, ((gy % 4 == 0 && gy % 100 != 0) || (gy % 400 == 0))? 29: 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+   for (gm = 0; gm < 13; gm++){
+    v = sal_a[gm];
+
+    if (gd <= v) break;
+
+    gd -= v;
+   }
+
+   return (new Date(gy + '/' + gm + '/' + gd));
+ },
+ _convertDigitsToMoneyFormat: (n, c, d, t) => {
+    var c = isNaN(c = Math.abs(c)) ? 2 : c,
+      d = d == undefined ? "." : d,
+      t = t == undefined ? "," : t,
+      s = n < 0 ? "-" : "",
+      i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+      j = (j = i.length) > 3 ? j % 3 : 0;
+
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+  },
   _convertKeywordToToken: (keyword) => {
     return keyword.replace(/_/ig, ' ').replace(/\b\w/ig, char => char.toUpperCase());
   },
