a1de5269ee9a016deb88786755d85a473d938465
Ali Alizade Haqiqi <alializade@peedes.com>
2019-06-05 18:24:44 +0430

    An issue has been fixed.

--- assets/flows/states/reducers/views/authentication/index.js
+++ assets/flows/states/reducers/views/authentication/index.js
@@ -1,9 +1,11 @@
 import ForgottenPassword from './fogotten-password';
 import Signup from './signup';
+import Login from './login';
 import VerifyEmail from './verify-email';
 
 module.exports = {
   ForgottenPassword,
   Signup,
+  Login,
   VerifyEmail
 };
